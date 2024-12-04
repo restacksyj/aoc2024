d1e774fc92c1700f929a71b20c2857dc5a1197ee
restacksyj <restacksyj@gmail.com>
2024-12-04 03:12:50 -0500

    beautified day 4 part 2 a little

--- 4/4_2.cpp
+++ 4/4_2.cpp
@@ -65,7 +65,7 @@ bool ogsearch(vector<vector<char>> &v, int row, int col, char c, string &temp,
 }
 
 int main() {
-  //ifstream f("input.txt");
+  // ifstream f("input.txt");
   ifstream f("input1.txt");
 
   if (!f.is_open()) {
@@ -89,83 +89,60 @@ int main() {
   // top left diagonal, col -2(sam or mas),
   int n = (int)v.size();
   int m = (int)v[0].size();
+  vector<vector<int>> vis(n, vector<int>(m, -1));
 
   for (int i = 0; i < (int)v.size(); i++) {
     for (int j = 0; j < (int)v[0].size(); j++) {
       char el = v[i][j];
       if (el == 'M') {
+        vis[i][j] = 1;
         vector<string> directions = {"bld", "brd", "urd", "uld"};
-        // vector<string> directions = {"bld"};
         for (auto it : directions) {
-          if (it == "bld") {
+          if (it == "bld" || it == "uld") {
             string temp = "";
             temp += 'M';
+            string newDir = it == "bld" ? "brd" : "urd";
             if (ogsearch(v, i, j, 'A', temp, ans, it)) {
               string newtemp = "S";
               bool res1 = false;
               bool res2 = false;
-              if (j - 2 >= 0 && j - 2 < m && v[i][j - 2] == 'S') {
-                res1 = search(v, i, j - 2, 'A', newtemp, ans, "brd");
+              if (j - 2 >= 0 && j - 2 < m && v[i][j - 2] == 'S'
+                  /*&& vis[i][j - 2] == -1*/
+              ) {
+                res1 = search(v, i, j - 2, 'A', newtemp, ans, newDir);
               }
               newtemp = "M";
-              if (j - 2 >= 0 && j - 2 < m && v[i][j - 2] == 'M') {
-                res2 = search(v, i, j - 2, 'A', newtemp, ans, "brd");
+              if (j - 2 >= 0 && j - 2 < m && v[i][j - 2] == 'M' 
+                                /*&& vis[i][j - 2] == -1*/
+                            ) {
+                res2 = search(v, i, j - 2, 'A', newtemp, ans, newDir);
               }
               if (res1 || res2) {
+                vis[i][j - 2] = 1;
                 ans++;
               }
             }
-          } else if (it == "brd") {
+          } else if (it == "brd" || it == "urd") {
             string temp = "";
             temp += 'M';
+            string newDir = it == "brd" ? "bld" : "uld";
             if (ogsearch(v, i, j, 'A', temp, ans, it)) {
               string newtemp = "S";
               bool res1 = false;
               bool res2 = false;
-              if (j + 2 >= 0 && j + 2 < m && v[i][j + 2] == 'S') {
-                res1 = search(v, i, j + 2, 'A', newtemp, ans, "bld");
+              if (j + 2 >= 0 && j + 2 < m && v[i][j + 2] == 'S'
+                  /*&& vis[i][j + 2] == -1*/
+              ) {
+                res1 = search(v, i, j + 2, 'A', newtemp, ans, newDir);
               }
               newtemp = "M";
-              if (j + 2 >= 0 && j + 2 < m && v[i][j + 2] == 'M') {
-                res2 = search(v, i, j + 2, 'A', newtemp, ans, "bld");
+              if (j + 2 >= 0 && j + 2 < m && v[i][j + 2] == 'M'
+                  /*&& vis[i][j + 2] == -1*/
+              ) {
+                res2 = search(v, i, j + 2, 'A', newtemp, ans, newDir);
               }
               if (res1 || res2) {
-                ans++;
-              }
-            }
-          } else if (it == "uld") {
-            string temp = "";
-            temp += 'M';
-            if (ogsearch(v, i, j, 'A', temp, ans, it)) {
-              string newtemp = "S";
-              bool res1 = false;
-              bool res2 = false;
-              if (j - 2 >= 0 && j - 2 < m && v[i][j - 2] == 'S') {
-                res1 = search(v, i, j - 2, 'A', newtemp, ans, "urd");
-              }
-              newtemp = "M";
-              if (j - 2 >= 0 && j - 2 < m && v[i][j - 2] == 'M') {
-                res2 = search(v, i, j - 2, 'A', newtemp, ans, "urd");
-              }
-              if (res1 || res2) {
-                ans++;
-              }
-            }
-          } else if (it == "urd") {
-            string temp = "";
-            temp += 'M';
-            if (ogsearch(v, i, j, 'A', temp, ans, it)) {
-              string newtemp = "S";
-              bool res1 = false;
-              bool res2 = false;
-              if (j + 2 >= 0 && j + 2 < m && v[i][j + 2] == 'S') {
-                res1 = search(v, i, j + 2, 'A', newtemp, ans, "uld");
-              }
-              newtemp = "M";
-              if (j + 2 >= 0 && j + 2 < m && v[i][j + 2] == 'M') {
-                res2 = search(v, i, j + 2, 'A', newtemp, ans, "uld");
-              }
-              if (res1 || res2) {
+                vis[i][j + 2] = 1;
                 ans++;
               }
             }
@@ -175,7 +152,8 @@ int main() {
     }
   }
 
-  cout << ans << endl;
+//ans comes double because we are repeately counting the elements that is why divide by 2
+  cout << ans/2<< endl;
 
   return 0;
 }
